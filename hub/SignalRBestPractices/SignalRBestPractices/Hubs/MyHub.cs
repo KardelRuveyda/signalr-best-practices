02c7e87197a5af1ea555911939a07f8d49f51d28
Ruveyda Cetin <ruveyda.cetin@d-teknoloji.com.tr>
2023-01-26 13:51:32 +0300

    SignalR Best Practices

---
+++ hub/SignalRBestPractices/SignalRBestPractices/Hubs/MyHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRServerExample.Hubs
+{
+    public class MyHub : Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
---
+++ hub/SignalRBestPractices/SignalRBestPractices/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using SignalRServerExample.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRBestPractices
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseWebSockets();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseRouting();
+
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials            
+
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<MyHub>("/myhub");
+            });
+        }
+    }
+}
